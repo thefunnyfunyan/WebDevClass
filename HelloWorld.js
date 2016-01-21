aa90ca3e0d50308349c58d8c3f3822edf91d884a
unknown <brando15@E1116W22.win.cis.ksu.edu>
2016-01-21 15:48:44 -0600

    Little bit more work not finished

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,13 +1,17 @@
 var http = require('http');
+var fs = require('fs');
 
 const PORT = 8080;
 
 function handleRequest(request, response) {
-  response.end("it works!");
+  console.log(request.url)
+  var data = fs.readFileSync(request.url);
+  response.write(fs.readFileSync("syllabus.html"));
+  response.write(fs.readFileSync("syllabus-a.css"));
 }
 
 var server = http.createServer(handleRequest);
 
 server.listen(PORT, function(){
-console.log("Started server on http://localhost:%s", PORT);
+  console.log("Started server on http://localhost:%s", PORT);
 });
